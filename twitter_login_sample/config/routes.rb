45ae9adb17832701a6c12fdde4acd2221baf2166
kzk-maeda <casino.music0711@gmail.com>
2020-07-17 09:09:40 +0900

    basic token authorization is aded

--- twitter_login_sample/config/routes.rb
+++ twitter_login_sample/config/routes.rb
@@ -1,5 +1,16 @@
+require 'devise_token_auth'
+
 Rails.application.routes.draw do
   root 'home#about'
   get 'home/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  namespace :api, defaults: { format: :json } do
+    scope :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }      
+    end
+  end
+
 end
